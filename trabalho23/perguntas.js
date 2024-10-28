296d526d0d40d0fa8daff0020716b3e86746ede8
b0mb4do <168562943+b0mb4do@users.noreply.github.com>
2024-10-28 12:32:15 -0300

    Add files via upload

---
+++ trabalho23/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'academia',
+    'como toma whey?',
+    'misture 20g de whey com 200ml de agua ou sua preferencia'
+)
+
+criaCartao(
+    'academia',
+    'usar o famoso suco realmente faz crescer?',
+    'sim,mas tera problemas na saude'
+)
+
+criaCartao(
+    'academia',
+    'qm e o atual mrs olympia?',
+    'sanson dauda'
+)
+
+criaCartao(
+    'academia',
+    'qm e o maior campeao do mrs olympia?',
+    'ronnie coleman(papai ronni)'
+)
